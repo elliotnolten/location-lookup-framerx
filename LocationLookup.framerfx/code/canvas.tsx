0ee330c2df6fe7720b84ac3f967586581ee2c1be
Elliot Nolten <elliot@nolten.co>
2019-08-07 09:59:37 +0200

    Removed ZoekBox Components

--- LocationLookup.framerfx/code/canvas.tsx
+++ LocationLookup.framerfx/code/canvas.tsx
@@ -1,5 +1,3 @@
 // WARNING: this file is auto generated, any changes will be lost
 import { createDesignComponent, CanvasStore } from "framer"
 const canvas = CanvasStore.shared(); // CANVAS_DATA;
-export const ZoekBox_None = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_mIAk96cvs", {}, 320,60);
-export const ZoekBox_Suggestion = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string,Count?:string,Info?:string,Name?:string}>(canvas, "id_hWS_UsnfQ", {Count:"string",Info:"string",Name:"string"}, 320,56);
